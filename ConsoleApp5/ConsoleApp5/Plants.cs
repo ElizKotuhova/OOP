81f13c26a6077ab52c78656a5c32b61c2032113b
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2020-04-23 00:43:39 +0300

    Add New Lab

---
+++ ConsoleApp5/ConsoleApp5/Plants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+   abstract class Plants
+    {
+        string type;
+        int view;
+
+        public abstract void care();
+
+        public void gettype()
+        {
+            Console.WriteLine(type);
+        }
+    }
+}
